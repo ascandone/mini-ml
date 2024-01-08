ff184088654b7fb7c118ee3db359723340e49d24
Alessandro Scandone <alescandone@gmail.com>
2024-01-08 01:04:38 +0100

    implemented parser

---
+++ src/parser.test.ts
@@ -0,0 +1,127 @@
+import { test, expect } from "vitest";
+import { SpannedAst, unsafeParse, Span } from "./parser";
+
+test("int number", () => {
+  const INPUT = "42";
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    type: "constant",
+    value: 42,
+    span: [0, 2],
+  });
+});
+
+test("float number", () => {
+  const INPUT = "42.3";
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    type: "constant",
+    value: 42.3,
+    span: [0, 4],
+  });
+});
+
+test("ident", () => {
+  const INPUT = "abc";
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    type: "ident",
+    ident: "abc",
+    span: [0, 3],
+  });
+});
+
+test("infix +", () => {
+  const INPUT = "1 + 2";
+  // == (+) 1 2
+  // == ((+) 1) 2
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    type: "application",
+    caller: {
+      type: "application",
+      caller: {
+        type: "ident",
+        ident: "+",
+        span: spanOf(INPUT, "+"),
+      },
+      arg: { type: "constant", value: 1, span: spanOf(INPUT, "1") },
+      span: spanOf(INPUT, INPUT),
+    },
+    arg: { type: "constant", value: 2, span: spanOf(INPUT, "2") },
+    span: spanOf(INPUT, INPUT),
+  });
+});
+
+test("infix *", () => {
+  const INPUT = "1 * 2";
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    type: "application",
+    caller: {
+      type: "application",
+      caller: {
+        type: "ident",
+        ident: "*",
+        span: spanOf(INPUT, "*"),
+      },
+      arg: { type: "constant", value: 1, span: spanOf(INPUT, "1") },
+      span: spanOf(INPUT, INPUT),
+    },
+    arg: { type: "constant", value: 2, span: spanOf(INPUT, "2") },
+    span: spanOf(INPUT, INPUT),
+  });
+});
+
+test("infix expr prec", () => {
+  const INPUT = "1 + 2 * 3";
+  //= 1 + (2 * 3)
+  //= `+` 1 (2 * 3)
+  //= (`+` 1) (2 * 3)
+  //= (`+` 1) (`*` 2 3)
+  //= (`+` 1) (`*` 2 3)
+  //= (`+` 1) ((`*` 2) 3)
+  //= (`+` 1) ((`*` 2) 3)
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    type: "application",
+    caller: {
+      type: "application",
+      caller: { type: "ident", ident: "+", span: expect.anything() },
+      arg: { type: "constant", value: 1, span: expect.anything() },
+      span: expect.anything(),
+    },
+    arg: {
+      type: "application",
+      caller: expect.anything(),
+      arg: { type: "constant", value: 3, span: expect.anything() },
+      span: expect.anything(),
+    },
+    span: expect.anything(),
+  });
+});
+
+test("let definition statement", () => {
+  const INPUT = `let x = 42 in k`;
+
+  expect(unsafeParse(INPUT)).toEqual<SpannedAst>({
+    span: [0, INPUT.length],
+    type: "let",
+    binding: { name: "x", span: spanOf(INPUT, "x") },
+    definition: {
+      type: "constant",
+      value: 42,
+      span: spanOf(INPUT, "42"),
+    },
+    body: {
+      type: "ident",
+      ident: "k",
+      span: spanOf(INPUT, "k"),
+    },
+  });
+});
+
+function spanOf(src: string, substr: string): Span {
+  const index = src.indexOf(substr);
+  return [index, index + substr.length];
+}
---
+++ src/parser.ts
@@ -0,0 +1,102 @@
+import type {
+  MatchResult,
+  NonterminalNode,
+  Node as OhmNode,
+  TerminalNode,
+} from "ohm-js";
+import grammar from "./parser/grammar.ohm-bundle";
+import { Ast } from "./ast";
+import { UntypedAst } from "./typecheck";
+
+export type Span = [startIdx: number, endIdx: number];
+
+export type SpannedAst<T = {}> = Ast<T & { span: Span }>;
+
+function getSpan({ source }: OhmNode): Span {
+  return [source.startIdx, source.endIdx];
+}
+
+const semantics = grammar.createSemantics();
+
+function infixOp(ident: string) {
+  return function (
+    this: NonterminalNode,
+    left: NonterminalNode,
+    op: TerminalNode,
+    right: NonterminalNode
+  ): SpannedAst {
+    return {
+      type: "application",
+      caller: {
+        type: "application",
+        caller: {
+          type: "ident",
+          ident,
+          span: getSpan(op),
+        },
+        arg: left.expr(),
+        span: getSpan(this),
+      },
+      arg: right.expr(),
+      span: getSpan(this),
+    };
+  };
+}
+
+semantics.addOperation<SpannedAst>("expr()", {
+  Exp_let(_let, ident, _eq, def, _in, body) {
+    return {
+      type: "let",
+      binding: { name: ident.sourceString, span: getSpan(ident) },
+      definition: def.expr(),
+      body: body.expr(),
+      span: getSpan(this),
+    };
+  },
+
+  AddExp_plus: infixOp("+"),
+  MulExp_times: infixOp("*"),
+
+  ident(_l, _ns) {
+    return {
+      type: "ident",
+      ident: this.sourceString,
+      span: getSpan(this),
+    };
+  },
+  number(_) {
+    return {
+      type: "constant",
+      value: Number(this.sourceString),
+      span: getSpan(this),
+    };
+  },
+});
+
+semantics.addOperation<UntypedAst[]>("parse()", {
+  MAIN_expr(a) {
+    return a.expr();
+  },
+});
+
+export type ParseResult<T> =
+  | { ok: true; value: T }
+  | { ok: false; matchResult: MatchResult };
+
+export function parse(input: string): ParseResult<SpannedAst> {
+  const matchResult = grammar.match(input);
+  if (matchResult.failed()) {
+    return { ok: false, matchResult };
+  }
+
+  return { ok: true, value: semantics(matchResult).parse() };
+}
+
+export function unsafeParse(input: string): SpannedAst {
+  const res = parse(input);
+  if (res.ok) {
+    return res.value;
+  }
+
+  throw new Error(`${res.matchResult.getInterval().getLineAndColumn()}`);
+}
